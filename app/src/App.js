c8202ae7fc9ae855738ba94328df402250be3fef
Sanjeevvarmabettter <sanjeevvarmacode@gmail.com>
2023-12-29 22:51:52 +0530

    added particles jsx

--- app/src/App.js
+++ app/src/App.js
@@ -1,30 +1,24 @@
 import React from "react";
 import Header from "./components/Header";
-import DarkModeToggleComponent from "./components/DarkModeToggle";
+import Particles from "react-particles";
+
+// import DarkModeToggleComponent from "./components/DarkModeToggle";
 // import Profilepic from "./components/Profilepic";
 // import Card from "./components/Card";
 
-import ToggleSwitch from "./components/ToggleSwitch";
+
 import ProfileCardContainer from "./components/ProfileCardContainer";
 import './App.css';
 
 function App() {
   
-  const [isDarkMode, setIsDarkMode] = useState(false);
-
-  const toggleDarkMode = () => {
-    setIsDarkMode(!isDarkMode);
-  }
-
   return (
     <div className="App">
-      <div className={isDarkMode ? 'dark-mode':'light-mode'}></div>
-      <DarkModeToggleComponent isDarkMode={isDarkMode} toggleDarkMode={toggleDarkMode} />
       <Header />
+      <Particles />
       {/* <Profilepic /> */}
       <ProfileCardContainer />
       {/* <Card /> */}
-      <ToggleSwitch />
       </div>
   
   );
