55293e0fbb54e23216790f39af8edbc8068df805
Sanjeevvarmabettter <sanjeevvarmacode@gmail.com>
2024-01-15 15:04:20 +0530

    adding projects

--- app/src/components/Header.js
+++ app/src/components/Header.js
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../App.css';
 
 function Header() {
+
+    const [showprojects,setshowprojects] = useState(false);
+
+    const handleprojectclick = () => {
+        setshowprojects(!showprojects);
+    };
+
     return (
         <header>
             <nav>
                 <a href='#'>Home</a>
-                <a href='#'>Projects</a>
-                <a href='#'>Blog</a>
+                <a href='#' onClick={handleprojectclick}>Projects</a>
+                <a href='http://localhost:8000'>Blog</a>
             </nav>
+
+            {showprojects && (
+                <div>
+                    Project 1: 
+                </div>
+            )}
         </header>
     );
 }
 
 export default Header;
+
+//if i want to display the informantion about my project i need to create a new state
